9277b6b55409f9a89789a2244b06c9e3472465b7
anjulituck@Anjulis-iMac.local <anjulituck@Anjulis-iMac.local>
2022-04-20 15:58:07 -0500

    added some script to start quiz but will need to revisit when I understand the concept more

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 # Project Description 
 
-We are currently making a responsive mutliple choice, timed quiz using Javascript. We havne't finished the Challenge since I am still learning the concepts.
+We are currently making a responsive mutliple choice, timed quiz using Javascript. We havne't finished the Challenge since I am still learning the concepts. I will refractor and add more to the code later when I have time. 
 
 # Deployed Links
 
-REPO:
+REPO: https://github.com/anjulituck/challengefour.git
 
-GITHUB PAGES:
+GITHUB PAGES: https://anjulituck.github.io/challengefour/
--- script.js
+++ script.js
@@ -72,3 +72,40 @@ restartBtn.addEventListener("click", restart);
 prevBtn.addEventListener("click", prev);
 nextBtn.addEventListener("click",next);
 submitBtn.addEventListener("click",submit);
+
+//function to begin Quiz 
+
+function beginQuiz() {
+    currentQuestion = 0;
+    totalScore.innerHTML = questions.length;
+    questionText.innerHTML = questions[currentQuestion].question;
+    trueBtn.innerHTML = questions[currentQuestion].answers[0].option;
+    trueBtn.onclick = () => {
+        if(questions[currentQuestion].answers[0].answer) {
+            if(score < 3) {
+                score++;
+            }
+        }
+        userScore.innerHTML = score;
+        if(currentQuestion < 2) {
+            next();
+        }
+    }
+  
+    falseBtn.innerHTML = questions[currentQuestion].answers[1].option;
+    falseBtn.onclick = () => {
+        if(questions[currentQuestion].answers[1].answer) {
+            if(score < 3) {
+                score++;
+            }
+        }
+        userScore.innerHTML = score;
+        if(currentQuestion < 2) {
+            next();
+        }
+    }
+  
+    prevBtn.classList.add("hide");
+ }
+  
+ beginQuiz();
